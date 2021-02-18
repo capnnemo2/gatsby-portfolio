17503b991b7adb9edd1efe7f17fbed09945a1c04
Ben Hernandez <benjammin234@gmail.com>
2021-02-17 20:15:58 -0800

    Added react-icons

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,24 @@
-import React from "react"
-import Layout from "../components/Layout"
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/Layout";
 
 export default () => (
   <Layout>
     <div className="content">
-      <p>I am a web developer emerging from the adventure tourism industry. I pride myself on my ability to think on my feet, my attention to detail, and my drive for efficiency. I enjoy applying the skills and knowledge I have gained from whitewater rafting and scuba diving to the creative process of web development.</p>
-      <p>Get in touch if you're curious how such seemingly incongruous activites as guiding a boat over a waterfall and creating web applications intersect.</p>
+      <p>
+        I am a web developer emerging from the adventure tourism industry. I
+        pride myself on my ability to think on my feet, my attention to detail,
+        and my drive for efficiency. I enjoy applying the skills and knowledge I
+        have gained from whitewater rafting and scuba diving to the creative
+        process of web development.
+      </p>
+      <p>
+        <Link to="/contact" className="about-contact-link" title="Contact Ben">
+          Get in touch{" "}
+        </Link>
+        if you're curious how such seemingly incongruous activites as guiding a
+        boat over a waterfall and creating web applications intersect.
+      </p>
       {/* insert email icon here */}
       {/* <p>
         This describes how awesome Ben is. For example, he rides{" "}
@@ -19,4 +32,4 @@ export default () => (
       </p> */}
     </div>
   </Layout>
-)
+);
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,6 @@
-import React from "react"
-import Layout from "../components/Layout"
+import React from "react";
+import Layout from "../components/Layout";
+import { FaEnvelopeSquare, FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 export default () => {
   return (
@@ -11,7 +12,46 @@ export default () => {
           correspondence. You can send an email, check him out on GitHub, and/or
           connect on LinkedIn.
         </p>
+        <p>
+          Don't be shy. Feel free to peruse my GitHub, check me out on LinkedIn,
+          and send me an email.
+        </p>
+        <div className="flex spread">
+          <a
+            aria-label="send Ben an email link"
+            href="mailto:benjammin234@gmail.com"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FaEnvelopeSquare
+              className="contact-icon"
+              title="Send Ben an email"
+            />
+          </a>
+          <a
+            aria-label="go to Ben's GitHub"
+            href="https://github.com/capnnemo2"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FaGithubSquare
+              className="contact-icon"
+              title="Check out Ben's GitHub"
+            />
+          </a>
+          <a
+            aria-label="Link to Ben's LinkedIn"
+            href="https://www.linkedin.com/in/ben--hernandez/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FaLinkedin
+              className="contact-icon"
+              title="Check Ben out on LinkedIn"
+            />
+          </a>
+        </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
